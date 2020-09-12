6ff24995976fbaf646fdaad3d95d09ec8720e3ea
Björn Büttner <Idrinth@users.noreply.github.com>
2020-09-12 14:55:46 +0200

    creating write logic adding basic merge.xml - paths need to be adjusted

--- src/main/java/de/idrinth/endlessspace2/modmerger/InitialController.java
+++ src/main/java/de/idrinth/endlessspace2/modmerger/InitialController.java
@@ -2,11 +2,14 @@ package de.idrinth.endlessspace2.modmerger;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.time.LocalTime;
 import java.util.HashMap;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.apache.commons.io.FileUtils;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
@@ -20,7 +23,7 @@ public class InitialController {
     //for testing assume we know what mods to merge - remember to subscribe
     String[] mods = {"2216068650", "932394338", "1224504743", "1263186686", "1301600120", "1316786885"};
     var workshopfolder = new File("C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\392110");
-    var assets = new HashMap<String, Byte[]>();
+    var assets = new HashMap<String, File>();
     var data = new HashMap<String, HashMap<String, String>>();
     for (var mod : mods) {
       load(new File(workshopfolder.getAbsoluteFile().toString() + '/' + mod), assets, data);
@@ -28,7 +31,7 @@ public class InitialController {
     write(data, assets);
   }
 
-  private void load(File folder, HashMap<String, Byte[]> assets, HashMap<String, HashMap<String, String>> data) {
+  private void load(File folder, HashMap<String, File> assets, HashMap<String, HashMap<String, String>> data) {
     for (var file : folder.listFiles()) {
       if (file.getName().endsWith(".xml")) {
         try {
@@ -45,16 +48,44 @@ public class InitialController {
       }
     }
   }
-  private void collect(File folder, HashMap<String, Byte[]> assets)
+  private void collect(File folder, HashMap<String, File> assets)
   {
-    
+    //anything not xml saved as filepath => File
   }
   private void read(Node config, File folder, HashMap<String, HashMap<String, String>> data)
   {
-    
+    //
   }
-  private void write(HashMap<String, HashMap<String, String>> data, HashMap<String, Byte[]> assets)
+  private void write(HashMap<String, HashMap<String, String>> data, HashMap<String, File> assets)
   {
-    
+    var name = "Merge" + LocalTime.now().toString();
+    var target = new File("~/Documents/Endless space 2/Community/" + name);
+    if (!target.isDirectory()) {
+      target.mkdirs();
+    }
+    for (var path : assets.keySet()) {
+      var out = new File(target.toString() + "/" + path);
+      out.getParentFile().mkdirs();
+      try {
+        FileUtils.copyFile(assets.get(path), out);
+      } catch (IOException ex) {
+      }
+    }
+    for (String type : data.keySet()) {
+      var out = new File(target.toString() + "/source/" + type + ".xml");
+      out.getParentFile().mkdirs();
+      try {
+        out.createNewFile();
+        for (String item : data.get(type).keySet()) {
+          FileUtils.write(out, item, Charset.defaultCharset(), true);
+        }
+      } catch (IOException ex) {
+      }
+    }
+    var modFile = new File(target.toString() + "/" + name + ".xml");
+    try {
+      FileUtils.copyInputStreamToFile(getClass().getResourceAsStream("Merge.xml"), modFile);
+    } catch (IOException ex) {
+    }
   }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module de.idrinth.endlessspace2.modmerger {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.xml;
+    requires org.apache.commons.io;
 
     opens de.idrinth.endlessspace2.modmerger to javafx.fxml;
     exports de.idrinth.endlessspace2.modmerger;
